80aada17d38cdde747abcc4c347b85804237144b
chuepapamyonaing <chuepapamyonaing@cybermissions.co.jp>
2021-10-22 19:51:26 +0900

    画面デザイン更新

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :item
 
-  validates :description, presence: true, length: { maximum: 1000 }
+  validates :description, presence: true, length: { maximum: 300 }
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,9 @@ class Item < ApplicationRecord
   has_many :comments, dependent: :destroy
   mount_uploader :image, ImageUploader
   validates :name, presence: true, length: { maximum: 50 }
-  validates :description, length: { maximum: 200 }  
+  validates :description, presence: true, length: { maximum: 300 }  
   validates :upload_date, presence: true
-  validate :image_size
+  validate  :image_size
   validates :image, presence: true
 
   private
