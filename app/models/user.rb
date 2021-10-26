5259051be97a09bb22782fa9021a0bc9b9aa68e2
chuepapamyonaing <chuepapamyonaing@cybermissions.co.jp>
2021-10-26 11:52:02 +0900

    ユーザー情報更新リンクとページ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, length: { maximum: 50 }
   has_many :items, dependent: :destroy
   has_many :comments, dependent: :destroy
 end
